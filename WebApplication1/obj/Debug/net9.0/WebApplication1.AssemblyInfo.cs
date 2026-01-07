3ee5a1cd21e34bf2c028974dcea991bf6e5faead
Stephan Balasuriya <stephan.effectivesolutions.com>
2026-01-07 11:36:09 +0530

    complete

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4c0f98c61a634fd647a700f64f0ec9cafe9153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2991afb6a9cda7c662ad5af1816281880bab0e8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
